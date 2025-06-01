56087fb3d81d8999258578d2ec5c3fd395ddc5a2
sruthichandalada <sruthichan789@gmail.com>
2025-06-01 11:51:50 +0530

    Update main.tf

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -1,17 +1,1 @@
-provider "aws" {
-    region = "ap-south-1"
-}
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket        = var.bucket_name
-  force_destroy = var.force_destroy
-  tags          = var.tags
-}
-
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.my_bucket.id
-
-  versioning_configuration {
-    status = var.versioning ? "Enabled" : "Suspended" 
-  }
-}
